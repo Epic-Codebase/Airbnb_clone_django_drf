dcce2470e9be213d579b447d738152782754d4bb
lmbelo <lucas.belo@live.com>
2025-01-31 23:06:26 -0300

    Book property

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -2,8 +2,8 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 from .forms import PropertyForm
-from .models import Property
-from .serializers import PropertiesDetailSerializer, PropertiesListSerializer
+from .models import Property, Reservation
+from .serializers import PropertiesDetailSerializer, PropertiesListSerializer, ReservationsListResializer
 
 
 @api_view(['GET'])
@@ -30,6 +30,19 @@ def properties_detail(request, pk):
     return JsonResponse(serializer.data)
 
 
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def property_reservations(request, pk):
+
+    property = Property.objects.get(pk=pk)
+    reservations = property.reservations.all()
+
+    serializer = ReservationsListResializer(reservations, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+
+
 @api_view(['POST', 'FILES'])
 def create_property(request):
 
@@ -50,3 +63,31 @@ def create_property(request):
         'success': False,
         'errors': form.errors.as_json()
     }, status=400)
+
+
+@api_view(['POST'])
+def book_property(request, pk):
+    try:
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        number_of_nights = request.POST.get('number_of_nights', '')
+        total_price = request.POST.get('total_price', '')
+        guests = request.POST.get('guests', '')
+
+        property = Property.objects.get(pk=pk)
+
+        reservation = Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_nights=number_of_nights,
+            total_price=total_price,
+            guests=guests,
+            created_by=request.user,
+        )
+
+        return JsonResponse({'success': True})
+    except Exception as e:
+        print('error', e)
+
+        return JsonResponse({'success': False})
